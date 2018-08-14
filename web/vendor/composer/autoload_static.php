c535e6b90d43bd3d03c103d7e32457ee4c571fe0
Reese Morgan <ramorgan212@gmail.com>
2018-08-14 15:03:30 -0400

    added address module

--- web/vendor/composer/autoload_static.php
+++ web/vendor/composer/autoload_static.php
@@ -131,6 +131,7 @@ class ComposerStaticInitDrupal8
         array (
             'Composer\\Semver\\' => 16,
             'Composer\\Installers\\' => 20,
+            'CommerceGuys\\Addressing\\' => 24,
         ),
         'B' => 
         array (
@@ -444,6 +445,10 @@ class ComposerStaticInitDrupal8
         'Composer\\Installers\\' => 
         array (
             0 => __DIR__ . '/..' . '/composer/installers/src/Composer/Installers',
+        ),
+        'CommerceGuys\\Addressing\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/commerceguys/addressing/src',
         ),
         'Behat\\Mink\\Driver\\' => 
         array (
